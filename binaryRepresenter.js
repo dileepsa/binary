215c1d87a43ce71be4bb1ce31d9e6f54cca8ccf9
Dileep A <99489054+dileepsa@users.noreply.github.com>
2022-05-08 12:37:28 +0530

    Modified binaryRepresenter.js
    Created one.
    Created zero.
    Added base to one.

--- binaryRepresenter.js
+++ binaryRepresenter.js
@@ -1,16 +1,42 @@
-const { createElement, createLink } = require('../generateHtml.js');
+const { createElement, createLink } = require('../createElement.js');
 const fs = require('fs');
 
-const binaryRepresenter = (binaryNum) => {
-  const color = +binaryNum === 0 ? 'red' : 'green';
-  return createElement({ element: 'div', content: '', classes: [color] })
+const zero = () => {
+  const innerDiv = createElement({
+    element: 'div',
+    content: '',
+    classes: ['zero-inner']
+  });
+
+  return createElement({
+    element: 'div',
+    content: innerDiv,
+    classes: ['zero-outer']
+  });
+};
+
+const one = () => {
+  const base = createElement({ element: 'div', content: '', classes: ['one-base'] });
+  const pole = createElement({ element: 'div', content: '', classes: ['one-pole'] })
+  const wrapper = createElement(
+    {
+      element: 'div',
+      content: pole + base,
+      classes: ['one-wrapper']
+    });
+  return wrapper;
+}
+
+const generateHtml = (binaryNum) => {
+  const html = +binaryNum === 0 ? zero() : one();
+  return html;
 };
 
 const main = (template, number) => {
   const binaryNumber = number.toString(2);
-  const binaryHtml = binaryNumber.split('').map(binaryRepresenter).join('');
+  const html = binaryNumber.split('').map(generateHtml).join('');
   let htmlStr = fs.readFileSync(template, 'utf-8');
-  htmlStr = htmlStr.replace(/__CONTENT__/, binaryHtml);
+  htmlStr = htmlStr.replace(/__CONTENT__/, html);
   fs.writeFileSync('index.html', htmlStr, 'utf-8');
 };
 
